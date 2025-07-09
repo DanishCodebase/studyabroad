fc5880c24ccf9004b51adc466f4230555135c6ef
DanishCodebase <ammarsaifi5376@gmail.com>
2025-07-09 16:56:35 +0530

    Update Header component layout; remove unnecessary flex class and adjust image visibility for better responsiveness.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,11 @@ import logo from "../assets/logo.svg";
 const Header = () => {
   return (
     <header className="container mx-auto max-w-7xl px-5 pt-6 flex sm:flex-row gap-4 justify-between items-center">
-      <div className="flex items-center gap-5 sm:gap-10">
+      <div className="items-center gap-5 sm:gap-10">
         <img
           src={logo}
           alt="Planet Education Logo"
-          className="h-9 sm:h-14 w-auto"
+          className="h-9 sm:h-14 sm:invisible w-auto"
         />
       </div>
     </header>
